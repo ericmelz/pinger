0013becb33212070ca3431dd04f70b2645c1bd25
Eric <emelz@gumgum.com>
2022-05-10 23:58:33 +0000

    added various locations

--- src/pinger.py
+++ src/pinger.py
@@ -2,10 +2,22 @@ import requests
 from time import sleep, time
 
 
-URL = 'http://verity-edge.lax.k8s.sx.ggops.com/xandr/page/classify?url=https://trilltrill.jp/articles/2247759'
+#URL = 'http://verity-edge.lax.k8s.sx.ggops.com/xandr/page/classify?url=https://trilltrill.jp/articles/2247759'
+#URL = 'http://localhost'
+#URL = 'http://15.253.6.74'
+#URL = 'http://70.224.227.122'
+#URL = 'http://13.52.219.80'
+#URL = 'http://54.188.245.97'
+URL = 'http://34.227.14.247'
 #DATA_OUTFILE = 'data_one_sec.csv'
 #DATA_OUTFILE = 'data_one_min.csv'
-DATA_OUTFILE = 'data.csv'
+#DATA_OUTFILE = 'data.csv'
+#DATA_OUTFILE = 'data_localhost.csv'
+#DATA_OUTFILE = 'data_same_localzone.csv'
+#DATA_OUTFILE = 'data_other_localzone.csv'
+#DATA_OUTFILE = 'data_us_west1.csv'
+#DATA_OUTFILE = 'data_us_west2.csv'
+DATA_OUTFILE = 'data_us_east1.csv'
 #SLEEP_TIME = .1  # ten pings per second
 SLEEP_TIME = 1  # one ping per second
 #SLEEP_TIME = 10  # one ping every 10 seconds
